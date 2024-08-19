3d64ad836b56000eb70675e8fb92e8f57af8be42
hardik123451 <hardikbalwani9@gmail.com>
2024-08-19 07:46:19 +0530

    till dictionary

--- chapter5/01_dict.py
+++ chapter5/01_dict.py
@@ -5,9 +5,9 @@ marks = {
     "list" : [1,2,9]
 }
 
-print (marks, type(marks))
-print (marks["hardik"])
-print (marks["list"])
+# print (marks, type(marks))
+# print (marks["hardik"])
+# print (marks["list"])
 
 
 
@@ -25,6 +25,31 @@ marks = {
 
 # print(marks.values())
 
-marks.update({"hardik": 99})
+marks.update({"hardik": 80, "rens" : 55})
 
-print(marks)
+# print(marks)
+
+# print(marks.get('rohan'))
+# print(marks['rohan'])
+
+
+
+
+dic = ['a', 'b', 'c']
+my_dict = dict.fromkeys(dic, 12 )
+# print(my_dict)
+
+
+
+
+# di = {'a': 1}
+# print(di.setdefault('a', 2))  # Output: 1
+# print(di.setdefault('b', 2))  # Output: 2
+# print(di.setdefault('c', 2))  # Output: 2
+# print(di)  # Output: {'a': 1, 'b': 2}
+
+
+
+
+
+
