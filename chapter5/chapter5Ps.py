6c624104520629bc805efefc15583343b9dcc36d
hardik123451 <hardikbalwani9@gmail.com>
2024-08-20 10:51:29 +0530

    tillPsSets

---
+++ chapter5/chapter5Ps.py
@@ -0,0 +1,46 @@
+# words = {
+#         "billi": "cat",
+#         'chuha': "rat",
+#         "kutta": "dog"
+#     }
+
+# requ = input()
+
+# print(words[requ])    # q1
+
+
+# n1 = input()
+# n2 = input()
+# n3 = input()
+# n4 = input()
+# n5 = input()
+# n6 = input()
+# n7 = input()
+# n8 = input()
+
+
+# s1 = set()
+
+# s1.add(n1)
+# s1.add(n2)
+# s1.add(n3)
+# s1.add(n4)
+# s1.add(n5)
+# s1.add(n6)
+# s1.add(n7)
+# s1.add(n8)
+
+# print(type(s1), s1)         # q2
+
+
+
+# set1  = {"18", 18}
+# print(set1)            #q4
+
+
+set2 = set()
+
+set2.add(20)
+set2.add(20.0)
+set2.add('20')
+print(set2 , len(set2))
