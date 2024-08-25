eb82d6ccfd7b99768631374e2fc43f7ffdebb64d
hardik123451 <hardikbalwani9@gmail.com>
2024-08-25 16:48:50 +0530

    till ch6

--- chapter6/03_multiple_if_statements.py
+++ chapter6/03_multiple_if_statements.py
@@ -1,1 +1,23 @@
-# hi
+
+
+a = int(input("Enter your age: "))
+
+# if statement no 1
+if(a%2 == 0):
+    print("a is even")
+
+# if statement no 2
+if(a>=18):
+    print('you are an adult')
+    print('Good for you')
+
+elif(a<0):
+    print("Write your correct age")
+
+elif(a==0):
+    print("Age cannot be 0 (zero)")
+
+else:
+    print("you are a minor")
+
+print('End of program')
---
+++ chapter6/chapter6Ps.py
@@ -0,0 +1,165 @@
+# find greatest number in 4 numbers
+
+# a1 = int(input())
+# a2 = int(input())
+# a3 = int(input())
+# a4 = int(input())
+
+# # hardik answer
+# # newList = []
+# # newList.append(a1)
+# # newList.append(a2)
+# # newList.append(a3)
+# # newList.append(a4)
+# # print(max(newList))
+
+
+# # harry answer    and not statement added by me
+# if(a1>a2 and a1>a3 and a1>a4 and a1 != a2 and a1 != a2 and a1 != a3):
+#     print('a1 is greater', a1)
+
+# elif(a2>a1 and a2>a3 and a2>a4 and a2 != a1 and a3 != a3 and a2 != a4):
+#     print('a2 is greater', a2)
+
+# elif(a3>a1 and a3>a2 and a3>a4 and a3 != a1 and a3 != a2 and a3 != a4):
+#     print('a3 is greater' , a3)
+
+# elif(a4>a1 and a4>a2 and a4>a3 and a4 != a1 and a4 != a2 and a4 != a3):
+#     print('a4 is greater' , a4)
+
+
+# else: 
+#     print('some values are equal')         #q1
+
+
+
+
+# find student passed or failed  total 40% required   and each subject 33% required
+
+# marks1 = int(input('enter marks 1: '))
+# marks2 = int(input('enter marks 2: '))
+# marks3 = int(input('enter marks 3: '))
+
+# #check for total percentage
+# totalMarks = marks3 + marks1 + marks2
+
+# totalPercent = totalMarks/300*100
+
+# if(totalPercent >= 40 and marks1 > 33 and marks2 > 33 and marks3 > 33):
+#     print(marks1, marks2, marks3, totalPercent, 'passed')
+
+# else:
+#     print(marks1, marks2, marks3, totalPercent, 'failed')   # q2
+
+
+# p1 = "make a lot of money"
+# p2 = 'buy now'
+# p3 = 'subscribe this'
+# p4 = 'click this'
+
+# message = input("Enter your comment: ")
+
+# if(p1 in message or p2 in message or p3 in message or p4 in message):
+#     print('its a spam')
+
+# else:
+#     print('this is ok')      # q3
+
+
+
+
+# username = input()
+# if(len(username) < 10):
+#     print('short')
+# else:
+#     print("its OK")      #q4
+
+
+
+# listOfNames = ["hardik", "harry", "sunny", "sonu", "monu", "ponu"]
+
+# aName = input()
+
+# if(aName in listOfNames):
+#     print("present")
+
+# else:
+#     print("absent")     #q5
+
+
+# m1 = int(input())
+
+# if(m1 > 90 and m1 <= 100):
+#     print("Ex")
+# elif(m1 > 80 and m1 <= 90):
+#     print("A")
+# elif(m1 > 70 and m1 <= 80):
+#     print("B")
+# elif(m1 > 60 and m1 <= 70):
+#     print("C")
+
+# elif(m1 >= 50 and m1 <= 60):
+#     print("D")
+
+# else:
+#     print("F", m1)    # q6
+
+
+
+
+post  =  input()
+post = post.capitalize()
+
+if("Harry" in post):
+    print("Harry is in the post, success")
+
+else:
+    print("Harry is missing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
