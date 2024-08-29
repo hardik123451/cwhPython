de8c22765215fa315e2393150c502101407fd2aa
hardik123451 <hardikbalwani9@gmail.com>
2024-08-30 02:38:22 +0530

    oops

---
+++ chapter10/01_class.py
@@ -0,0 +1,15 @@
+def getSurname():
+    print("hi")
+    
+class Employee:   # noun
+    name = "Harry"   # attribute
+    language = 'Py'   # attribute
+    salary = 1200000   # attribute
+    surname = getSurname()  #verbs
+
+
+
+
+harry = Employee()
+
+print(harry.name, harry.language)
--- chapter9/chapter9Ps.py
+++ chapter9/chapter9Ps.py
@@ -13,30 +13,157 @@
 # you need to read "Hi-score.txt" file may be blank 
 # or contains previous Hi-score. and write new hi-score init
 
-import random
+# import random
 
-def game():
-    return random.randint(1, 62)
+# def game():
+#     yourScore = random.randint(1,62)
 
-currScore = game()
-print(currScore)
-f = open("Hi-score.txt")
+#     with open("Hi-score.txt") as f:
+#         hiScore = f.read()
 
-hi = f.read()
-if(hi==""):
-    hi = 0
-else:
-    hi = int(hi)
+#     if(hiScore == ""):
+#         hiScore = 0
+#     else:
+#         hiScore = int(hiScore)
 
-f.close()
+#     print(hiScore, type(hiScore))
 
-if(hi < currScore):
-    f = open("Hi-score.txt","w")
-    f.write(str(currScore))
-    f.close()
-    print("you win", currScore, hi)
-else:
-    print("you lose", currScore, hi )
+#     if(yourScore>hiScore):
+#         with open("Hi-score.txt", "w") as f:
+#             f.write(str(yourScore))
+#         print("New Hi-Score, Congrats" ,yourScore)      
+#     else:
+#         print("You Lose", yourScore)
+
+# game()
 
 
-f.close()
+
+
+
+
+
+# # q3  generate multiply tables from 2 to 20 in different files
+
+# def generateTable(n):
+#     table = ""
+#     for i in range(1,11):
+#         table += f"{n} X {i} = {n*i}\n"
+    
+#     with open(f"tables/table_of_{n}.txt", "w") as f:
+#         f.write(table)
+
+# for i in range(2,21):
+#     generateTable(i)
+
+
+
+# # q4  a file contains Donkey multiple times , replace that word with '#####' by updating same file
+
+# word = "donkey"
+# with open("donkey.txt") as f:
+#     content = f.read()
+
+# contentNew = content.replace(word, "#####")
+
+# with open("donkey.txt", "w") as f:
+#     f.write(contentNew)
+
+
+
+# #q5  same as q4 here a list of words will be censored
+
+# words = ['donkey', 'bad', 'ganda']
+
+# with open("donkey.txt") as f:
+#     content = f.read()
+
+# for word in words:
+#     content = content.replace(word, "#"*len(word))
+
+# with open("donkey.txt", "w") as f:
+#     f.write(content)
+    
+
+
+# # q6   find word 'python' is present in log file ?
+
+# with open("log.txt") as f:
+#     content = f.read()
+
+# if("python" in content):
+#     print("present")
+# else:
+#     print("not present")
+
+
+
+# # q7 find on which line no. word 'python' is present
+
+# # line = 1
+# # word = 'python'
+# # with open("log.txt") as f:
+# #     lines = f.readlines()
+# #     countLines = len(lines)
+
+
+# # for i in lines:
+# #     if(word in i):
+# #         print(i.index(word))   # my try failed
+
+# with open("log.txt") as f:
+#     lines = f.readlines()
+
+# lineNo = 1
+# for line in lines:
+#     if('python' in line):
+#         print("present", lineNo)
+#         break
+#     lineNo += 1 
+# else:
+#     print("not present in all lines")
+
+
+
+
+
+# # q8 make duplicate file of this.txt file
+
+# with open('this.txt') as f:
+#     content = f.read()
+
+# with open('this2.txt', 'w') as f:
+#     f.write(content)
+
+
+
+# # q9 check both files are identical and matches content of each other or not
+
+# with open('this.txt') as f:
+#     content = f.read()
+
+# with open('this2.txt') as f:
+#     content2 = f.read()
+
+# if(content == content2):
+#     print("yes, matched")
+# else:
+#     print("not matched")
+
+
+
+
+# # q10 wipe out content of a file
+
+# with open('this2.txt', 'w') as f:
+#     f.write("")
+
+
+
+# q11     rename file
+
+with open('old.txt') as f:
+    content = f.read()
+
+with open('new.txt', 'w') as f:
+    f.write(content)
